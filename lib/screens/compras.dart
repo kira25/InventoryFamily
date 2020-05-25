47587533d85fbc3239f93f92852ec95ea3fb94d1
Erick <erick@pucp.pe>
2020-05-25 16:44:44 -0500

    first commit

--- lib/screens/compras.dart
+++ lib/screens/compras.dart
@@ -58,11 +58,7 @@ class _ComprasState extends State<Compras> {
                   Container(
                     child: GestureDetector(
                       onTap: _letsPress,
-                      child: buttonPressed
-                          ? ButtonTapped(
-                              icon: Icons.home,
-                            )
-                          : MyButton(icon: Icons.home),
+                      child: buttonPressed ? ButtonTapped(icon: Icons.home,) : MyButton(icon: Icons.home),
                     ),
                   ),
                 ],
